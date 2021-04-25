1279330d03b261429dcdc25650b2edbe15c9eec9
zdraffko <zdravkomb1@gmail.com>
2021-04-25 12:08:31 +0300

    Added the and registered the db context.

---
+++ src/DataPanda.Persistence/DataPandaDbContext.cs
@@ -0,0 +1,37 @@
+﻿using DataPanda.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataPanda.Persistence
+{
+    public class DataPandaDbContext : DbContext
+    {
+        public DataPandaDbContext(DbContextOptions<DataPandaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ActivityLog> ActivityLogs { get; set; }
+
+        public DbSet<Assignment> Assignments { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Enrolment> Enrolments { get; set; }
+
+        public DbSet<FileSubmission> FileSubmissions { get; set; }
+
+        public DbSet<Grade> Grades { get; set; }
+
+        public DbSet<LearningPlatform> LearningPlatforms { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<StudentEnrolment> StudentEnrolments { get; set; }
+
+        public DbSet<StudentEnrolmentAssignment> StudentEnrolmentAssignments { get; set; }
+
+        public DbSet<StudentEnrolmentWiki> StudentEnrolmentWikis { get; set; }
+
+        public DbSet<Wiki> Wikis { get; set; }
+    }
+}
--- src/DataPanda.Startup/Startup.cs
+++ src/DataPanda.Startup/Startup.cs
@@ -1,16 +1,9 @@
+using DataPanda.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DataPanda.Startup
 {
@@ -23,18 +16,10 @@ namespace DataPanda.Startup
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+            => services
+                .AddPersistence(Configuration);
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DataPanda.Startup", Version = "v1" });
-            });
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
