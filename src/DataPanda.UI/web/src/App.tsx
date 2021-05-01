898312246c9b2286dc081c282e66c377ebcf43f0
zdraffko <zdravkomb1@gmail.com>
2021-05-01 14:33:44 +0300

    Added eslint and absolute imports.

--- src/DataPanda.UI/web/src/App.tsx
+++ src/DataPanda.UI/web/src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './App.css';
-import Menu from "../src/components/Menu/Menu";
-import { menuOptions } from "../src/models/menuOption";
+import React from "react";
+import "App.css";
+import Menu from "components/Menu/Menu";
+import { menuOptions } from "models/menuOption";
 
 function App() {
-  return (
-    <div>
-      < Menu menuOptions={menuOptions} />
-    </div>
-  );
+	return (
+		<div>
+			<Menu menuOptions={menuOptions} />
+		</div>
+	);
 }
 
 export default App;
--- src/DataPanda.UI/web/src/components/Menu/Menu.tsx
+++ src/DataPanda.UI/web/src/components/Menu/Menu.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import styles from "../Menu/Menu.module.scss";
-import { IMenuOption } from "../../models/menuOption";
+import { IMenuOption } from "models/menuOption";
+import styles from "./Menu.module.scss";
 
 interface IProps {
-    menuOptions: IMenuOption[];
+	menuOptions: IMenuOption[];
 }
 
-const Menu : React.FC<IProps> = ({ menuOptions }) => {
-    return(
-    <div className={styles.Menu}>
-        {menuOptions.map((option) => (
-            <button type="button" className={styles.Button} key={option.id}>{option.name}</button>
-        ))}
-    </div>
-  )};
-  
-  export default Menu;
+const Menu : React.FC<IProps> = ({ menuOptions }) => (
+	<div className={styles.Menu}>
+		{menuOptions.map(option => (
+			<button type="button" className={styles.Button} key={option.id}>{option.name}</button>
+		))}
+	</div>
+);
+
+export default Menu;
