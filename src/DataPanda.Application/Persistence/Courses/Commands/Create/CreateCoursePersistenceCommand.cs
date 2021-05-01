3b11ddea90f54df71af69cbafe46c812f8178709
zdraffko <zdravkomb1@gmail.com>
2021-05-01 22:31:33 +0300

    Added a persistence command for creating a course.

---
+++ src/DataPanda.Application/Persistence/Courses/Commands/Create/CreateCoursePersistenceCommand.cs
@@ -0,0 +1,16 @@
+﻿using DataPanda.Application.Contracts.CQRS.Commands;
+using DataPanda.Application.Contracts.CQRS.Results;
+using DataPanda.Domain.Entities;
+
+namespace DataPanda.Application.Persistence.Courses.Commands.Create
+{
+    public class CreateCoursePersistenceCommand : IPersistenceCommand<Result>
+    {
+        public CreateCoursePersistenceCommand(Course course)
+        {
+            Course = course;
+        }
+
+        public Course Course { get; }
+    }
+}
--- src/DataPanda.Startup/IoC/Persistence/PersistenceServices.cs
+++ src/DataPanda.Startup/IoC/Persistence/PersistenceServices.cs
@@ -1,4 +1,8 @@
-﻿using DataPanda.Persistence;
+﻿using DataPanda.Application.Contracts.CQRS.Commands;
+using DataPanda.Application.Contracts.CQRS.Results;
+using DataPanda.Application.Persistence.Courses.Commands.Create;
+using DataPanda.Persistence;
+using DataPanda.Persistence.Entities.Courses.Commands.Create;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,12 +12,17 @@ namespace DataPanda.Startup.IoC.Persistence
     public static class PersistenceServices
     {
         public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
-            => services.AddDatabase(configuration);
+            => services
+                .AddDatabase(configuration)
+                .AddCommands();
 
         private static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
             => services.AddDbContext<DataPandaDbContext>(options
                 => options.UseSqlServer(
                     configuration.GetConnectionString("DefaultConnection"),
                     sqlServer => sqlServer.MigrationsAssembly(typeof(DataPandaDbContext).Assembly.FullName)));
+
+        private static IServiceCollection AddCommands(this IServiceCollection services)
+            => services.AddScoped<IPersistenceCommandHandler<CreateCoursePersistenceCommand, Result>, CreateCoursePersistenceCommandHandler>();
     }
 }
