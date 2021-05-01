364b3d571b8db5e031f88d05ccf2f67417f5dfaf
zdraffko <zdravkomb1@gmail.com>
2021-05-01 22:34:14 +0300

    Added meta data properties for the file in the upload command.

--- src/DataPanda.Application/Features/Files/Commands/Upload/UploadFileCommand.cs
+++ src/DataPanda.Application/Features/Files/Commands/Upload/UploadFileCommand.cs
@@ -6,11 +6,32 @@ namespace DataPanda.Application.Features.Files.Commands.Upload
 {
     public class UploadFileCommand : ICommand<Result>
     {
-        public UploadFileCommand(Stream fileStream)
+        public UploadFileCommand(
+            Stream fileStream,
+            string platformName,
+            string platformType,
+            string platformUrl,
+            string courseName,
+            string courseFieldOfApplication)
         {
             FileStream = fileStream;
+            PlatformName = platformName;
+            PlatformType = platformType;
+            PlatformUrl = platformUrl;
+            CourseName = courseName;
+            CourseFieldOfApplication = courseFieldOfApplication;
         }
 
         public Stream FileStream { get; }
+
+        public string PlatformName { get; }
+
+        public string PlatformType { get; }
+
+        public string PlatformUrl { get; }
+
+        public string CourseName { get; }
+
+        public string CourseFieldOfApplication { get; }
     }
 }
--- src/DataPanda.Application/Features/Files/Commands/Upload/UploadFileCommandHandler.cs
+++ src/DataPanda.Application/Features/Files/Commands/Upload/UploadFileCommandHandler.cs
@@ -1,22 +1,40 @@
 ﻿using DataPanda.Application.Contracts.CQRS.Commands;
 using DataPanda.Application.Contracts.CQRS.Results;
 using DataPanda.Application.Contracts.Parsers;
+using DataPanda.Application.Features.Files.Models;
+using DataPanda.Application.Persistence.Courses.Commands.Create;
+using DataPanda.Domain.Entities;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace DataPanda.Application.Features.Files.Commands.Upload
 {
     public class UploadFileCommandHandler : ICommandHandler<UploadFileCommand, Result>
     {
-        private readonly IStudentResultParser studentResultParser;
+        private readonly IParser<IEnumerable<StudentResult>> studentResultParser;
+        private readonly IPersistenceCommandHandler<CreateCoursePersistenceCommand, Result> createCoursePersistenceCommandHandler;
 
-        public UploadFileCommandHandler(IStudentResultParser studentResultParser)
+        public UploadFileCommandHandler(
+            IParser<IEnumerable<StudentResult>> studentResultParser,
+            IPersistenceCommandHandler<CreateCoursePersistenceCommand, Result> createCoursePersistenceCommandHandler)
         {
             this.studentResultParser = studentResultParser;
+            this.createCoursePersistenceCommandHandler = createCoursePersistenceCommandHandler;
         }
 
         public async Task<Result> Handle(UploadFileCommand command)
         {
             var result = await studentResultParser.Parse(command.FileStream);
+
+            var course = new Course(command.CourseName, command.CourseFieldOfApplication);
+            var res = await createCoursePersistenceCommandHandler.Handle(new CreateCoursePersistenceCommand(course));
+
+            var learningPlatform = new LearningPlatform(command.PlatformName, command.PlatformType, command.PlatformUrl);
+
+            foreach (var studentResult in result.SuccessPayload)
+            {
+                var enrolment = new Enrolment(course.Id, learningPlatform.Id);
+            }
 
             return Result.Success();
         }
