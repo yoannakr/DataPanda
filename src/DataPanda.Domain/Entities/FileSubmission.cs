570626b5bf9e16344314af57027782c680a9bb5e
zdraffko <zdravkomb1@gmail.com>
2021-05-03 21:00:14 +0300

    Added NumberOfFiles column to the FileSubmissions table.

--- src/DataPanda.Domain/Entities/FileSubmission.cs
+++ src/DataPanda.Domain/Entities/FileSubmission.cs
@@ -2,9 +2,18 @@
 {
     public class FileSubmission
     {
+        public FileSubmission(int id, int enrolmentAssignmentId, int numberOfFiles)
+        {
+            Id = id;
+            EnrolmentAssignmentId = enrolmentAssignmentId;
+            NumberOfFiles = numberOfFiles;
+        }
+
         public int Id { get; set; }
 
         public int EnrolmentAssignmentId { get; set; }
+
+        public int NumberOfFiles { get; set; }
 
         public EnrolmentAssignment EnrolmentAssignments { get; set; }
     }
