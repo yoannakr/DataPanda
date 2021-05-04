b194a424966633be830230d9b348a1458500c3af
yoannakr <jkrmilkova@gmail.com>
2021-05-04 19:16:38 +0300

    Change file options to radio button

--- src/DataPanda.UI/web/src/components/FileUploader/FileOption/FileOption.tsx
+++ src/DataPanda.UI/web/src/components/FileUploader/FileOption/FileOption.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import styles from "./FileOption.module.scss";
 import { IOption } from "../../../models/option";
-import Button from "../../common/Button/Button";
 
 interface IProps {
 	fileOptions: IOption[];
+	selectOption: any;
+	selectedOption: IOption | undefined;
 }
 
-const FileOption: React.FC<IProps> = ({ fileOptions }) => (
+const FileOption: React.FC<IProps> = ({ fileOptions, selectOption, selectedOption }) => (
 	<div className={styles.Container}>
 		{fileOptions.map(option => (
-			<Button key={option.id} content={option.name} />
+			<div className={styles.FileOptions}>
+				<input type="radio" id={option.name} name="fileOptions" value={option.id} onChange={() => selectOption(option.id)} checked={selectedOption?.id === option.id} />
+				<label htmlFor={option.name} key={option.id}>
+					{option.name}
+				</label>
+			</div>
 		))}
 	</div>
 );
