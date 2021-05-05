b839bc2208cbabb5b76e6688cc414ea6beebeeff
yoannakr <jkrmilkova@gmail.com>
2021-05-05 13:16:58 +0300

    Add router

--- src/DataPanda.UI/web/src/App.tsx
+++ src/DataPanda.UI/web/src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "App.css";
 import Menu from "components/Menu/Menu";
-import { menuOptions } from "models/menuOption";
+import FileUploader from "./components/FileUploader/FileUploader";
 
 function App() {
 	return (
-		<div>
-			<Menu menuOptions={menuOptions} />
-		</div>
+		<Router>
+			<Switch>
+				<Route path="/" exact component={Menu} />
+				<Route path="/fileUploader" exact component={FileUploader} />
+			</Switch>
+		</Router>
 	);
 }
 
